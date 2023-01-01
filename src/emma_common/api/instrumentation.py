689e539768998fa8ade7e5ceb3364f560b790ac2
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-01-01 13:52:59 +0000

    feat!(telemetry): include many more attributes for the service
    This should allow for easily seeing what versions of the service are running, which instance of a service broke, and more.
    
    This is a breaking change because there are more variables that need to be provided to the `instrument_app` function and they must be provided as kwargs now

--- src/emma_common/api/instrumentation.py
+++ src/emma_common/api/instrumentation.py
@@ -4,7 +4,6 @@ This module makes handling instrumentation for observability much easier. This w
 providing the relevant packages are installed. If they're not installed, the various functions will
 do nothing.
 """
-
 from fastapi import FastAPI
 from loguru import logger
 from opentelemetry import trace
@@ -13,14 +12,28 @@ from opentelemetry.instrumentation.botocore import BotocoreInstrumentor
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from opentelemetry.instrumentation.httpx import HTTPXClientInstrumentor
 from opentelemetry.instrumentation.logging import LoggingInstrumentor
-from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.resources import SERVICE_NAME, SERVICE_NAMESPACE, SERVICE_VERSION, Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
+from emma_common.aws.ec2 import create_resource_attributes
 
-def setup_tracing(service_name: str, otlp_endpoint: str) -> None:
+
+def create_resource(service_name: str, service_version: str, service_namespace: str) -> Resource:
+    """Create Resource for OTEL."""
+    # Prepare the resource attributes
+    resource_attributes = create_resource_attributes()
+    resource_attributes[SERVICE_NAME] = service_name
+    resource_attributes[SERVICE_VERSION] = service_version
+    resource_attributes[SERVICE_NAMESPACE] = service_namespace
+
+    # Create the resource
+    resource = Resource.create(attributes=resource_attributes)
+    return resource
+
+
+def setup_tracing(otlp_endpoint: str, resource: Resource) -> None:
     """Setup tracing for the API."""
-    resource = Resource.create(attributes={SERVICE_NAME: service_name})
     exporter = OTLPSpanExporter(endpoint=otlp_endpoint, insecure=True)
     provider = TracerProvider(resource=resource)
     provider.add_span_processor(BatchSpanProcessor(exporter))
@@ -35,9 +48,21 @@ def run_instrumentation(app: FastAPI) -> None:
     FastAPIInstrumentor.instrument_app(app)
 
 
-def instrument_app(app: FastAPI, service_name: str, otlp_endpoint: str) -> None:
+def instrument_app(
+    app: FastAPI,
+    *,
+    otlp_endpoint: str,
+    service_name: str,
+    service_version: str,
+    service_namespace: str,
+) -> None:
     """Instrument the API."""
-    setup_tracing(service_name=service_name, otlp_endpoint=otlp_endpoint)
+    resource = create_resource(
+        service_name=service_name,
+        service_version=service_version,
+        service_namespace=service_namespace,
+    )
+    setup_tracing(otlp_endpoint=otlp_endpoint, resource=resource)
     run_instrumentation(app)
 
     logger.info("Application instrumented successfully.")
---
+++ src/emma_common/aws/ec2.py
@@ -0,0 +1,80 @@
+import json
+from contextlib import suppress
+from functools import cache
+from pathlib import Path
+from typing import Any
+
+from opentelemetry.sdk.resources import Attributes
+from opentelemetry.semconv.resource import ResourceAttributes
+
+
+@cache
+def load_ec2_instance_metadata() -> dict[str, Any]:
+    """Try to load the metadata for the current EC2 instance.
+
+    This will raise an FileNotFoundError if the file does not exist, which will happen if we are
+    not running on an Ubuntu-based EC2 instance.
+    """
+    metadata_file = Path("/run/cloud-init/instance-data.json")
+    if not metadata_file.exists():
+        raise FileNotFoundError("Metadata does not exist.")
+
+    return json.loads(metadata_file.read_text())
+
+
+def create_resource_attributes() -> Attributes:
+    """Create attributes dict for the resource.
+
+    We try to fill in as many attributes as possible, ignoring those that are not available.
+    """
+    try:
+        metadata = load_ec2_instance_metadata()
+        instance_metadata = metadata["v1"]
+    except FileNotFoundError:
+        return {}
+
+    attributes: Attributes = {}
+    attributes[ResourceAttributes.OS_TYPE] = "linux"
+
+    with suppress(KeyError):  # noqa: WPS204
+        attributes[ResourceAttributes.SERVICE_INSTANCE_ID] = instance_metadata["instance_id"]
+        attributes[ResourceAttributes.HOST_ID] = instance_metadata["instance_id"]
+
+    with suppress(KeyError):
+        attributes[ResourceAttributes.CLOUD_PROVIDER] = instance_metadata["cloud_name"]
+
+    with suppress(KeyError):
+        attributes[ResourceAttributes.CLOUD_AVAILABILITY_ZONE] = instance_metadata[
+            "availability_zone"
+        ]
+
+    with suppress(KeyError):
+        attributes[ResourceAttributes.CLOUD_REGION] = instance_metadata["region"]
+
+    with suppress(KeyError):
+        attributes[
+            ResourceAttributes.CLOUD_PLATFORM
+        ] = f"{instance_metadata['cloud_name']}_{instance_metadata['platform']}"
+
+    with suppress(KeyError):
+        attributes[ResourceAttributes.HOST_NAME] = instance_metadata["host_name"]
+
+    with suppress(KeyError):
+        attributes[ResourceAttributes.HOST_TYPE] = metadata["meta-data"]["instance-type"]
+
+    with suppress(KeyError):
+        attributes[ResourceAttributes.HOST_ARCH] = instance_metadata["machine"].split("_")[0]
+
+    with suppress(KeyError):
+        attributes[ResourceAttributes.HOST_IMAGE_ID] = metadata["meta-data"]["ami-id"]
+
+    with suppress(KeyError):
+        attributes[ResourceAttributes.OS_DESCRIPTION] = instance_metadata["system_platform"]
+
+    with suppress(KeyError):
+        attributes[ResourceAttributes.OS_NAME] = instance_metadata["variant"]
+
+    with suppress(KeyError):
+        attributes[ResourceAttributes.OS_VERSION] = instance_metadata["distro_version"]
+
+    return attributes
